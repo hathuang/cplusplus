acd0e4e9a0f89b97308b37e44af30187d6e25923
Zhong Huang <zhohuang@sigma-rt.com>
2018-05-03 17:41:34 +0800

    using namespace 3_1.cpp

---
+++ 3_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::string;
+
+int main(int argc, const char *argv[])
+{
+	string s;
+
+	cin >> s;
+	//cout << s;
+
+	std::cout << s;
+
+	return 0;
+}
